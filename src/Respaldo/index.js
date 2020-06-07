dddefbe6835e6fad888b5fbed8e20405214d5611
MiPersona <bgva2005@yahoo.com>
2020-06-07 18:12:00 -0500

    0002_ParaVerificarLevantaGrafico

--- src/Respaldo/index.js
+++ src/Respaldo/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+
 import * as serviceWorker from './serviceWorker';
+import TutorialFormik from "./components/formik";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"; // Archivo CSS de Bootstrap 4
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"; // Archivo Javascript de Bootstrap 4
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TutorialFormik />
   </React.StrictMode>,
   document.getElementById('root')
 );
